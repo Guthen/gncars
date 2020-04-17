18f4e421b15be1879309c10c1f50f4a55b37c731
Guthen <33220603+Guthen@users.noreply.github.com>
2020-04-17 09:56:05 +0200

    🔨 Functionnal vehicles.json

--- lua/gncars/sv_vehicle.lua
+++ lua/gncars/sv_vehicle.lua
@@ -1,11 +1,28 @@
 GNCars.Vehicles = GNCars.Vehicles or {}
 
+--  > fetch vehicles data
+http.Fetch( "https://raw.githubusercontent.com/Guthen/gncars/master/vehicles.json", function( raw )
+    local vehicles = util.JSONToTable( raw )
+    if not vehicles then return error( "GNCars: failed to fetch vehicles data" ) end
+
+    --  > parse array positions to vectors
+    for k, v in pairs( vehicles ) do
+        for i, v in ipairs( v.seats ) do
+            vehicles[k].seats[i] = Vector( unpack( v ) )
+        end
+        v.nodraw = v.nodraw == nil and true or v.nodraw
+        GNCars.AddVehicle( k, v.seats, v.nodraw )
+    end
+
+    print( "GNCars: success to fetch vehicles data" )
+end )
+
 function GNCars.AddVehicle( vehicle_class, seats, nodraw )
-    if not list.Get( "Vehicles" )[vehicle_class] then return error( ( "'%s' is not a valid vehicle class!" ):format( vehicle_class ), 2 ) end
+    if not list.Get( "Vehicles" )[vehicle_class] then return --[[ error( ( "GNCars: '%s' is not a valid vehicle class!" ):format( vehicle_class ), 2 ) ]] end
 
     GNCars.Vehicles[vehicle_class] = { seats = seats, nodraw = nodraw == nil and true or nodraw }
 end
---GNCars.AddVehicle( "ast_db5tdm", { Vector( -15, -6, 15 ), Vector( -11, -38, 15 ), Vector( 11, -38, 15 ) } )
+--[[ GNCars.AddVehicle( "m1tdm", { Vector( 18, -0, 8 ) } ) ]]
 
 --  > add passenger seats
 hook.Add( "OnEntityCreated", "gncars", function( ent )
